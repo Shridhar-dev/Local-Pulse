b0071ed15b382a5ec8b48c7ce365562ee03994b7
Rushikesh-24 <rushikeshgaonkar24@gmail.com>
2024-10-17 11:22:32 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+import Footer from "../components/Footer";
 import Working from "@/components/Working";
 
 export default function Home() {
   return (
     <div className="font-grotesque">
       <Working />
+      <Footer/>
     </div>
   );
 }
